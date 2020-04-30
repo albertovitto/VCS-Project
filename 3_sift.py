88a2e9fb505c9f3d28de576d4e26114b97d2a836
albertovitto <albertovitto16@gmail.com>
2020-04-30 15:23:10 +0200

    first

---
+++ 3_sift.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+
+
+# https://stackoverflow.com/questions/60065707/cant-use-sift-in-opencv-v4-20/60066177#60066177
+
+# https://www.pyimagesearch.com/2018/09/19/pip-install-opencv/
+# https://aishack.in/tutorials/sift-scale-invariant-feature-transform-features/
+# https://stackoverflow.com/questions/29133085/what-are-keypoints-in-image-processing
+image = cv.imread("OpenCV-Python Tutorials/home.jpg")
+gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+sift = cv.xfeatures2d_SIFT.create()
+keyPoints = sift.detect(image, None)
+keypoints, descriptor = sift.compute(gray_image, keyPoints)
+
+output = cv.drawKeypoints(image, keyPoints, None)
+
+cv.imshow("FEATURES DETECTED", output)
+cv.imshow("NORMAL", image)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
