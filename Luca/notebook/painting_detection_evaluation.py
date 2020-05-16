cf55fd04d1079298b1feb4a0f3e5c8f3aae64ad3
Luca Denti <36813207+dentiluca@users.noreply.github.com>
2020-05-16 12:22:24 +0200

    Luca - painting detection evalution

---
+++ Luca/notebook/painting_detection_evaluation.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+
+from Luca.vcsp.painting_detection.evaluation import create_random_dict_for_test_set, read_dict_for_test_set, \
+    create_test_set
+
+
+if __name__ == '__main__':
+
+    # RUN ONCE
+    # d = create_random_dict_for_test_set()
+    # print(d)
+    # copy d in read_dict_test_set
+
+    dict = read_dict_for_test_set()
+
+    create_test_set(dict)
