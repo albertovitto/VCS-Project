1a10b3734e04900f6ec865c8b27179af970efbfb
cristian_mercadante <crimerca96@gmail.com>
2020-05-17 12:26:33 +0200

    Cristian: Yolo class that returns list of people bounding boxes. (see Cristian/YOLOv3/test_yolo.py)

---
+++ Cristian/YOLOv3/test_yolo.py
@@ -0,0 +1,174 @@
+import time
+
+import numpy as np
+import cv2
+
+from Luca.vcsp.painting_retrieval.retrieval import PaintingRetrieval
+from Luca.vcsp.painting_detection.detection import get_bb
+from Luca.vcsp.utils.multiple_show import show_on_row
+
+import torch
+from torch.autograd import Variable
+import random
+from Cristian.YOLOv3.util import *
+from Cristian.YOLOv3.darknet import Darknet
+from Cristian.YOLOv3.preprocess import prep_image, inp_to_image, letterbox_image
+
+
+def get_test_input(input_dim, CUDA):
+    img = cv2.imread("dog-cycle-car.png")
+    img = cv2.resize(img, (input_dim, input_dim))
+    img_ = img[:, :, ::-1].transpose((2, 0, 1))
+    img_ = img_[np.newaxis, :, :, :] / 255.0
+    img_ = torch.from_numpy(img_).float()
+    img_ = Variable(img_)
+    if CUDA:
+        img_ = img_.cuda()
+    return img_
+
+
+class Yolo():
+    def __init__(self, confidence=0.5, nms_thesh=0.4, num_classes=80):
+        self.confidence = confidence
+        self.nms_thesh = nms_thesh
+        self.CUDA = torch.cuda.is_available()
+        self.num_classes = num_classes
+        self.classes = load_classes('data/coco.names')
+        self.bbox_attrs = 5 + self.num_classes
+        print("Loading network.....")
+        self.model = Darknet('cfg/yolov3.cfg')
+        self.model.load_weights('yolov3.weights')
+        print("Network successfully loaded")
+        self.model.net_info["height"] = "416"
+        self.inp_dim = int(self.model.net_info["height"])
+        assert self.inp_dim % 32 == 0
+        assert self.inp_dim > 32
+        if self.CUDA:
+            self.model.cuda()
+        self.model(get_test_input(self.inp_dim, self.CUDA), self.CUDA)
+        self.model.eval()
+
+    @staticmethod
+    def prep_image(img, inp_dim):
+        """
+        Prepare image for inputting to the neural network.
+
+        Returns a Variable
+        """
+        orig_im = img
+        dim = orig_im.shape[1], orig_im.shape[0]
+        img = (letterbox_image(orig_im, (inp_dim, inp_dim)))
+        img_ = img[:, :, ::-1].transpose((2, 0, 1)).copy()
+        img_ = torch.from_numpy(img_).float().div(255.0).unsqueeze(0)
+        return img_, orig_im, dim
+
+    def retrieve_people_bb(self, frame):
+        img, orig_im, dim = self.prep_image(frame, self.inp_dim)
+
+        im_dim = torch.FloatTensor(dim).repeat(1, 2)
+
+        if self.CUDA:
+            im_dim = im_dim.cuda()
+            img = img.cuda()
+
+        with torch.no_grad():
+            output = self.model(Variable(img), self.CUDA)
+        output = write_results(output, self.confidence, self.num_classes, nms=True, nms_conf=self.nms_thesh)
+
+        im_dim = im_dim.repeat(output.size(0), 1)
+        scaling_factor = torch.min(self.inp_dim / im_dim, 1)[0].view(-1, 1)
+
+        output[:, [1, 3]] -= (self.inp_dim - scaling_factor * im_dim[:, 0].view(-1, 1)) / 2
+        output[:, [2, 4]] -= (self.inp_dim - scaling_factor * im_dim[:, 1].view(-1, 1)) / 2
+
+        output[:, 1:5] /= scaling_factor
+
+        for i in range(output.shape[0]):
+            output[i, [1, 3]] = torch.clamp(output[i, [1, 3]], 0.0, im_dim[i, 0])
+            output[i, [2, 4]] = torch.clamp(output[i, [2, 4]], 0.0, im_dim[i, 1])
+
+        bb = []
+        for x in output:
+            x1 = int(x[1].item())
+            y1 = int(x[2].item())
+            x2 = int(x[3].item())
+            y2 = int(x[4].item())
+            w = x2 - x1
+            h = y2 - y1
+            bb.append((x1, y1, w, h))
+
+        return bb
+
+
+if __name__ == '__main__':
+
+    db_dir_path = '../../dataset/paintings_db/'
+    files_dir_path = '../../dataset/'
+
+    retrieval = PaintingRetrieval(db_dir_path, files_dir_path)
+    retrieval.train()
+
+    # video_name = '000/VIRB0393.MP4'
+    # video_name = '001/GOPR5826.MP4'
+    # video_name = '005/GOPR2045.MP4'
+    # video_name = '012/IMG_4086.MOV'
+    # video_name = '005/GOPR2051.MP4'
+    # video_name = '004/IMG_3803.MOV'
+    # video_name = '008/VIRB0419.MP4'
+    # video_name = '008/VIRB0427.MP4'
+    # video_name = '012/IMG_4080.MOV'
+    video_name = '002/20180206_114720.mp4'
+
+    video_path = '../../dataset/videos/%s' % video_name
+
+    video = cv2.VideoCapture(video_path)
+
+    if not video.isOpened():
+        print("Error: video not opened correctly")
+
+    lost_frames = 0
+    pos_frames = 0
+    skip_frames = True
+
+    yolo = Yolo()
+
+    while video.isOpened():
+        ret, frame = video.read()
+
+        if ret:
+            people_bb = yolo.retrieve_people_bb(frame)
+            print(people_bb)
+            output, rois = get_bb(frame, include_steps=False)
+            for bb in people_bb:
+                x, y, w, h = bb
+                cv2.rectangle(output, (x, y), (x + w, y + h), (0, 0, 255))
+            cv2.imshow("Painting detection", output)
+
+            key = cv2.waitKey(1)
+            if key == ord('q'):  # quit
+                break
+            if key == ord('p'):  # pause
+                cv2.waitKey(-1)
+            if key == ord('r'):  # show rois with image retrieval
+                for i, roi in enumerate(rois):
+                    rank, _ = retrieval.predict(roi)
+                    cv2.putText(roi, "{}".format(i), (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3,
+                                False)
+                    print("Roi {} - rank = {}".format(i, rank))
+                    ground_truth = cv2.imread('../../dataset/paintings_db/' + "{:03d}.png".format(rank[0]))
+                    cv2.imshow("Roi {}".format(i), show_on_row(roi, ground_truth))
+                cv2.waitKey(-1)
+                for i, roi in enumerate(rois):
+                    cv2.destroyWindow("Roi {}".format(i))
+
+            if skip_frames:
+                pos_frames += video.get(cv2.CAP_PROP_FPS)
+                video.set(cv2.CAP_PROP_POS_FRAMES, pos_frames)
+        else:
+            lost_frames += 1
+            if lost_frames > 10:
+                print("Too many errors reading video or video ended")
+                break
+
+    video.release()
+    cv2.destroyAllWindows()
