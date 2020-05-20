ad784e8454465abda45e76c405c967ead0f5516c
Luca Denti <36813207+dentiluca@users.noreply.github.com>
2020-05-20 12:46:17 +0200

    Luca - some tests for people localization

--- Luca/vcsp/utils/multiple_show.py
+++ Luca/vcsp/utils/multiple_show.py
@@ -47,6 +47,8 @@ def show_on_row(img1, img2):
     output[0:img1.shape[0], 0:img1.shape[1], :] = img1
     output[0:img2.shape[0], img1.shape[1]:, :] = img2
 
+    output = resize_to_fit(output)
+
     return output
 
 
@@ -59,6 +61,27 @@ def show_on_col(img1, img2):
     output[0:img1.shape[0], 0:img1.shape[1], :] = img1
     output[img1.shape[0]:, 0:img2.shape[1]:, :] = img2
 
+    output = resize_to_fit(output)
+
+    return output
+
+
+def resize_to_fit(img):
+    h, w = img.shape[0:2]
+    dist_w = max(0, w - 1920)
+    dist_h = max(0, h - 1080)
+
+    if dist_h == 0 and dist_w == 0:
+        return img
+
+    if dist_h > dist_w:
+        scale_factor = (1 - dist_h / h)
+    else:
+        scale_factor = (1 - dist_w / w)
+
+    width = int(w * scale_factor)
+    height = int(h * scale_factor)
+    output = cv2.resize(img, (width, height))
     return output
 
 
