Cristian: some fix to people_localization.py. Details: - normalizing areas and distances - added sqrt weighting mode - refactorying

Cristian/image_processing/people_localization.py
@@ -7,40 +7,47 @@ CENTER_DISTANCE = 0
 # weight mode
 NONE = 0
 AREA = 1
+SQRT_AREA = 2
 
 
 def get_weights(painting_bbs, mode=AREA):
+    def area(bb):
+        x, y, w, h = bb
+        return w * h
+
     W = []
     if mode == AREA:
-        for bb in painting_bbs:
-            x, y, w, h = bb
-            area = w * h
-            W.append(area)
+        W = np.asarray([area(bb) for bb in painting_bbs])
+    elif mode == SQRT_AREA:
+        W = np.sqrt(np.asarray([area(bb) for bb in painting_bbs]))
     else:
-        pass
+        W = np.asarray(W)
 
-    W = np.asarray(W)
-    # using negative exponential to avoid w = 0
-    temperature = np.mean(W)
-    if temperature == 0:
-        raise ValueError("All paintings have 0 area")
-    W = np.exp(-W / temperature)
     print("W   = {}".format(W))
     return W
 
 
 def apply_weights(distances, painting_bbs, mode=AREA):
     assert len(distances) == len(painting_bbs)
-    weights = [1 / len(distances) for bb in distances]
     if mode != NONE:
         weights = get_weights(painting_bbs, mode=mode)
-        distances *= weights
+        # normalizing weights between 0 and 1,
+        # otherwise multiplication may have very high results
+        # and big gaps
+        weights = weights / np.amax(weights)
+        # using negative exponential in order to:
+        # - normalize between 0 and 1
+        # - the closest the painting, the higher the score
+        d = np.exp(-np.asarray(distances) / np.mean(distances))
+        weights *= d
+    else:
+        weights = np.exp(-np.asarray(distances) / np.mean(distances))
 
-    print("D*W = {}".format(distances))
-    return distances, weights
+    print("D*W = {}".format(weights))
+    return weights
 
 
-def person_paintings_distances(person_bb, painting_bbs, metric=CENTER_DISTANCE, weighting_mode=NONE):
+def person_paintings_distances(person_bb, painting_bbs, metric=CENTER_DISTANCE):
     def center(bb):
         x, y, w, h = bb
         return np.asarray((x + w // 2, y + h // 2))
@@ -55,35 +62,31 @@ def person_paintings_distances(person_bb, painting_bbs, metric=CENTER_DISTANCE, 
     return distances
 
 
-def get_room_id(distances, painting_retrievals, voting=False):
-    assert len(distances) == len(painting_retrievals)
+def get_room_id(weights, painting_retrievals, voting=False):
+    assert len(weights) == len(painting_retrievals)
     path = '../../dataset/data.csv'
     if not voting:
         # FROM NUMPY DOCS:
         # In case of multiple occurrences of the minimum values,
         # the indices corresponding to the first occurrence are returned
-        index = np.argmin(distances)
+        index = np.argmax(weights)
         painting_id = painting_retrievals[index]
         _, _, room = get_painting_info_from_csv(painting_id, path=path)
         return room
     else:
         votes = np.zeros(shape=(22))
-        max = np.amax(distances)
         for i, pr in enumerate(painting_retrievals):
             _, _, room = get_painting_info_from_csv(pr, path=path)
-            votes[room] += (-distances[i] + max + 1)
-
-        room = np.argmax(votes)
-
-        return room + 1
+            votes[room - 1] += weights[i]
+        room = np.argmax(votes) + 1
+        return room
 
 
-def localize_person(person_bb, painting_bbs, painting_retrievals,
-                    distance_metric=CENTER_DISTANCE, weighting_mode=AREA, voting=True):
-    assert len(painting_bbs) == len(painting_retrievals)
-    distances = person_paintings_distances(person_bb, painting_bbs, metric=distance_metric)
-    distances, weights = apply_weights(distances, painting_bbs, mode=weighting_mode)
-    room = get_room_id(distances, painting_retrievals, voting=voting)
+def localize_person(person_bb, painting_bbs, retrievals, distance=CENTER_DISTANCE, weighting=SQRT_AREA, voting=True):
+    assert len(painting_bbs) == len(retrievals)
+    distances = person_paintings_distances(person_bb, painting_bbs, metric=distance)
+    weights = apply_weights(distances, painting_bbs, mode=weighting)
+    room = get_room_id(weights, retrievals, voting=voting)
     return room
 
 
@@ -91,8 +94,8 @@ def main():
     # person = (270, 100, 250, 300)
     person = (1200, 100, 100, 100)
     paintings = [(600, 100, 100, 200), (1000, 50, 50, 100), (10, 700, 1000, 10)]
-    retrievals = [0, 1, 2]
-    room = localize_person(person, paintings, retrievals)
+    retrievals = [0, 1, 2]  # rooms 19 21 20
+    room = localize_person(person, paintings, retrievals, distance=CENTER_DISTANCE, weighting=SQRT_AREA, voting=True)
     print("Room: {}".format(room))
 
 